4799cf8e421f35f4b028fc400a426c4c37c92d5a
Antonio Breyer <antoniofabiopbreyer@gmail.com>
2022-08-13 23:41:15 -0300

    Versao Bonus Final

--- script.js
+++ script.js
@@ -23,6 +23,23 @@ function transformaN(palavraN){
         }
     }return Number(numS)
 }
+function transformaS(numero){
+    const numero_string=String(numero)
+    let numero_final= ''
+    for (let i=0; i<numero_string.length; i++){
+        if (numero_string[i] === '.'){
+            numero_final+=','
+        }
+        else{
+            numero_final+=numero_string[i]
+        }
+    }
+    //10,0
+    if(numero_final.length === 4){
+        numero_final+='0'
+    }
+    return 'R$'+numero_final
+}
 
 function liga(seletor){
     const classe=cortaS(seletor)
@@ -53,20 +70,16 @@ function pegandoInfo(){
         return[[nomeC.innerHTML,precoC.innerHTML],[nomeB.innerHTML,precoB.innerHTML],[nomeS.innerHTML,precoS.innerHTML]]
     }    
 }
+let Total=0
 function ligarFinal(){
     if (pegandoInfo() !== undefined){
-        const botao=document.querySelector('.teste')
+        const botao=document.querySelector('.but')
         botao.classList.remove('fundo-cinza')
         botao.classList.add('fundo-verde')
 
         const trocaP=document.querySelector('.final')
         trocaP.innerHTML='Fechar pedido'
-        const Total=transformaN(pegandoInfo()[0][1])+transformaN(pegandoInfo()[1][1])+transformaN(pegandoInfo()[2][1])
-        /*
-        let texto=`Olá,%20gostaria%20de%20fazer%20o%20pedido:%0A-%20Prato:%20${pegandoInfo()[0][0]}%0A-%20Bebida:%20${pegandoInfo()[1][0]}%0A-%20Sobremesa:%20${pegandoInfo()[2][0]}%0ATotal:%20R$%20${Total}`
-        const linkando=document.querySelector('.linkar')
-        linkando.href='https://wa.me/5521998320863?text='+texto
-        */
+        Total=transformaN(pegandoInfo()[0][1])+transformaN(pegandoInfo()[1][1])+transformaN(pegandoInfo()[2][1])
     }
 }
 function janelaEndereco(){
@@ -79,5 +92,48 @@ function cancela(seletor){
     const doc=document.querySelector(seletor)
     doc.classList.add('some')
 }
+let Endereco1=''
+let Nome1=''
 
+
+function paginaFinal(){
+    const nome=document.querySelector('.nomepessoa')
+    const endereco=document.querySelector('.ende')
+    const ultima_pagina=document.querySelector('.confirma-tudo')
+    if(nome.value !== ''  && endereco.value  !== ''){
+        Endereco1=endereco.value
+        Nome1=nome.value
+        ultima_pagina.classList.remove('some')
+        let texto=`Olá,%20gostaria%20de%20fazer%20o%20pedido:%0A-%20Prato:%20${pegandoInfo()[0][0]}%0A-%20Bebida:%20${pegandoInfo()[1][0]}%0A-%20Sobremesa:%20${pegandoInfo()[2][0]}%0ATotal:%20R$%20${Total}%0ANome:%20${Nome1}%0AEndereço:%20${Endereco1}`
+        const linkando=document.querySelector('.linkar')
+        linkando.href='https://wa.me/5521998320863?text='+texto
+    }
+}
+function confirmandoTudo(){
+    const pedido=pegandoInfo()
+    const fc=document.querySelector('.C1')
+    const fcp=document.querySelector('.P1')
+    fc.innerHTML=pedido[0][0]
+    fcp.innerHTML=pedido[0][1]
+
+    const fb=document.querySelector('.B1')
+    const fbp=document.querySelector('.P2')
+    fb.innerHTML=pedido[1][0]
+    fbp.innerHTML=pedido[1][1]
+
+    const fs=document.querySelector('.S1')
+    const fsp=document.querySelector('.P3')
+    fs.innerHTML=pedido[2][0]
+    fsp.innerHTML=pedido[2][1]
+
+    const ft=document.querySelector('.P4')
+    ft.innerHTML=transformaS(Total)
+
+
+    const NF=document.querySelector('.NomeF')
+    NF.innerHTML='Nome: '+Nome1
+    const EF=document.querySelector('.EndeF')
+    EF.innerHTML='Endereço: '+Endereco1
+
+}
 
